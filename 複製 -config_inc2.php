e80d2b40d7a2aedd472cf5b7621948f2b74565a7
waynehuang <cwh.cwh@gmail.com>
2023-06-21 13:16:54 +0800

    Main1
    First

---
+++ 複製 -config_inc2.php
@@ -0,0 +1,37 @@
+<?php
+	$g_hostname = 'localhost';
+	$g_db_type = 'mysql';
+	$g_database_name = 'mantis_AA123';
+	$g_db_username = 'root';
+	$g_db_password = '123456';
+
+$g_smtp_host = 'smtp.aoci.com.tw'; #SMTP服?器;
+	$g_smtp_connection_mode = '';     #不加密填'',否?根据??填'ssl'或者'tls'
+	$g_smtp_username = 'Mantis_oatc';    #?箱用?名
+	$g_smtp_password = 'Mantis';    #?箱密?
+	$g_administrator_email  = 'cairong.xu@aoci.com.tw';
+	$g_from_email = 'Mantis_oatc@aoci.com.tw';
+	$g_use_phpMailer = ON; # 使用 PHPMailer ?送?件 
+	$g_phpMailer_path ='C:\xampp\htdocs\PHPMailer_v5.1';
+	$g_phpMailer_method= PHPMAILER_METHOD_SMTP ;
+	//$g_smtp_connection_mode = 'ssl';
+	//$g_smtp_port = 25;
+	
+	/*
+	$g_phpMailer_method= PHPMAILER_METHOD_SMTP ;
+	
+	$g_smtp_host= 'smtp.aoci.com.tw';
+	$g_smtp_username = 'Mantis_oatc';
+	$g_smtp_password = 'Mantis';
+	$g_phpMailer_method =2;
+	/*
+	$g_smtp_connection_mode = 'ssl';
+	
+	$g_administrator_email  = 'chungwei_huang@aoci.com.tw'; 
+	$g_smtp_port = 25;
+	*/
+	
+
+
+
+?>
